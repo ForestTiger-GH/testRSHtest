2580152457ccc8220fffe0d2d56345e4d64562ca
Dmitry <80126225+ForestTiger-GH@users.noreply.github.com>
2024-10-14 17:46:26 +0300

    Update abc.py

--- testing/abc.py
+++ testing/abc.py
@@ -1,12 +1,4 @@
 # -*- coding: utf-8 -*-
 
-import numpy
-import pandas
-
-def cbr_screen(homedisk):
-    sub = 'Макроэкономика и банки/Банк России/Количество офисов банков (M)'
-    link = homedisk + sub
-    return link
-
 def fa_sum(a, b):
     return a + b
