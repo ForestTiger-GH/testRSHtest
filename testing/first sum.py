9ebd74aebc409f4e6b3112c3875dfcad926aad59
Dmitry <80126225+ForestTiger-GH@users.noreply.github.com>
2024-10-14 16:56:56 +0300

    Update first sum.py

--- testing/first sum.py
+++ testing/first sum.py
@@ -1,7 +1,9 @@
-# -*- coding: utf-8 -*-
-
-import numpy
-import pandas
-
 def f_sum(a, b):
     return a + b
+
+
+
+def cbr_screen(home_disk):
+  sub = 'Макроэкономика и банки/Банк России/Количество офисов банков (M)'
+  link = home_disk + sub
+  return link
