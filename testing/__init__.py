1921f025adbf202f17b254ee719f91b8f4943328
Dmitry <80126225+ForestTiger-GH@users.noreply.github.com>
2024-10-14 16:07:00 +0300

    Update __init__.py

--- testing/__init__.py
+++ testing/__init__.py
@@ -5,11 +5,5 @@ class Verytesting:
         self.r = a
         self.i = b
 
-
-
-def __init__(self, color, brand):
-    self.color = color
-    self.brand = brand
-
-def f_sum(a, b):
-    return a + b
+    def f_sum(a, b):
+        return a + b
