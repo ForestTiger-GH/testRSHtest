c57c42ab233aa047519b28bbb0f0cc97940b8cf9
Dmitry <80126225+ForestTiger-GH@users.noreply.github.com>
2024-10-14 17:15:24 +0300

    Update first sum.py

--- testing/first sum.py
+++ testing/first sum.py
@@ -1,9 +1,10 @@
+def cbr_screen(home_disk):
+    sub = 'Макроэкономика и банки/Банк России/Количество офисов банков (M)'
+    link = home_disk + sub
+    return link
+    
 def fi_sum(a, b):
     return a + b
 
 
 
-def cbr_screen(home_disk):
-    sub = 'Макроэкономика и банки/Банк России/Количество офисов банков (M)'
-    link = home_disk + sub
-    return link
