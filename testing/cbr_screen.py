78d49e07ffbdda247e970fbd06f0719bb546b295
Dmitry <80126225+ForestTiger-GH@users.noreply.github.com>
2024-10-15 15:35:49 +0300

    Update and rename main.py to cbr_screen.py

---
+++ testing/cbr_screen.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+def cbr_screener(homedisk):
+    sub = 'Макроэкономика и банки/Банк России/Количество офисов банков (M)'
+    link = homedisk + sub
+    return link
--- testing/main.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-
-def f_sum(a, b):
-    return a + b
